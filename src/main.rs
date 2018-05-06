2316ff6e27731f9caefc75089ea705c37d3da24f
kirjavascript <snkenjoi@gmail.com>
2018-05-06 01:39:29 +0100

    styles and stuff!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+// cakeybar
 extern crate gio;
 extern crate gtk;
 extern crate gdk;
@@ -5,32 +6,57 @@ extern crate chrono;
 
 use gio::prelude::*;
 use gtk::prelude::*;
+use gdk::{Screen, ScreenExt};
+use gtk::{
+    Box,
+    Label,
+    Orientation,
+    Image,
+    CssProvider,
+    STYLE_PROVIDER_PRIORITY_APPLICATION,
+};
 use std::env::args;
+use std::path::Path;
 use chrono::Local;
 
 fn current_time() -> String {
-    return format!("{}", Local::now().format("%Y-%m-%d %H:%M:%S"));
+    return format!("{}", Local::now().format("hello world 🍰 %Y-%m-%d %H:%M:%S"));
 }
 
+
+
 fn build_ui(application: &gtk::Application) {
-    let window = gtk::Window::new(gtk::WindowType::Toplevel); // Popup
+    let mut window = gtk::Window::new(gtk::WindowType::Toplevel); // use Popup for dmenu
     application.add_window(&window);
 
-    window.set_title("rustybar");
-    window.set_border_width(10);
-    window.set_default_size(1080, 20);
-    window.set_type_hint(gdk::WindowTypeHint::Dock);
-    window.move_(0, 1000000);
+    let (height, width) = (Screen::height(), Screen::width());
+    let screen = Screen::get_default().unwrap();
+    let mut monitors = Vec::new();
+    for i in 0..screen.get_n_monitors() {
+        monitors.push(screen.get_monitor_geometry(i));
+    }
+    println!("{:#?}", monitors);
 
-    // set screen
+    window.set_title("cakeybar");
+    window.set_default_size(0, 27);
+    window.set_type_hint(gdk::WindowTypeHint::Dock);
+    window.move_(0, height / 2);
+    // set screen by manipulating start position
+
+
+    let container = Box::new(Orientation::Horizontal, 0);
+
+    let img: Image = Image::new_from_file(Path::new("./resources/icon.svg"));
+    // img.set_name("icon");
+    container.add(&img);
 
     let time = current_time();
-    let label = gtk::Label::new(None);
+    let label = Label::new(None);
     label.set_text(&time);
+    label.set_margin_left(10);
+    container.add(&label);
 
-    window.add(&label);
-
-    // window.show_all();
+    window.add(&container);
     window.show_all();
 
     let tick = move || {
@@ -40,12 +66,36 @@ fn build_ui(application: &gtk::Application) {
     };
 
     gtk::timeout_add_seconds(1, tick);
+
+    // styles
+
+    let style_context = window.get_style_context().unwrap();
+    // let style = include_bytes!("../../style/command-input.css");
+    let style = r#"
+        window {
+            padding: 500px;
+            background-color: black;
+        }
+        label {
+            color: white;
+        }
+        #icon {
+            margin-bottom: 25px;
+        }
+    "#;
+    let provider = CssProvider::new();
+    match provider.load_from_data(style.as_bytes()) {
+        Ok(_) => {
+            style_context.add_provider(&provider, STYLE_PROVIDER_PRIORITY_APPLICATION);
+        },
+        Err(_) => println!("Error parsing stylesheet"),
+    };
 }
 
 fn main() {
 
     let application = gtk::Application::new(
-            "com.kirjava.rustybar",
+            "com.kirjava.cakeybar",
             gio::ApplicationFlags::empty(),
         )
         .expect("Initialization failed...");
