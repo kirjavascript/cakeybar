8a9e0507ccedf3263c377a45660bc930415e0768
kirjavascript <snkenjoi@gmail.com>
2018-05-12 23:48:28 +0100

    component parsing

--- src/config.rs
+++ src/config.rs
@@ -12,6 +12,7 @@ use std::collections::HashMap;
 pub struct Config {
     pub theme: Option<String>,
     pub bars: Vec<BarConfig>,
+    pub components: Vec<ComponentConfig>,
 }
 
 #[derive(Debug, Clone)]
@@ -32,17 +33,35 @@ pub struct BarConfig {
 pub struct ComponentConfig {
     pub name: String,
     pub type_: String,
-    pub properties: HashMap<String, Property>, //Value ?
+    pub properties: HashMap<String, Property>,
 }
 
 #[derive(Debug, Clone)]
 pub enum Property {
-    // String(String),
-    // Number(u64),
-    // Array(Vec<Property>)
+    String(String),
+    Number(u64),
+    Array(Vec<Property>),
 }
 
 pub fn parse_config(filename: &str) -> Config {
+    // create get_path closure
+    let config_dir = Path::new(filename).parent().unwrap();
+    let get_path = |f| {
+        // this can be improved (probably)
+        let file_path = Path::new(&f);
+        let file_path = if file_path.is_absolute() {
+            file_path.to_path_buf()
+        } else {
+            config_dir.join(&file_path)
+        }.canonicalize();
+        if file_path.is_err() {
+            eprintln!("{}: {:?}", &f, file_path.err().unwrap());
+            exit(2i32);
+        }
+        file_path.unwrap().as_path().to_str().unwrap_or("").to_string()
+    };
+
+    // get file
     let file_result = File::open(filename);
     if let Err(e) = file_result {
         eprintln!("{}: {}", filename, e);
@@ -54,6 +73,7 @@ pub fn parse_config(filename: &str) -> Config {
         .read_to_string(&mut contents)
         .expect("something went wrong reading the config");
 
+    // parse file
     let parsed_result = contents.parse::<toml::Value>();
 
     if let Err(e) = parsed_result {
@@ -62,6 +82,14 @@ pub fn parse_config(filename: &str) -> Config {
     }
 
     let parsed = parsed_result.unwrap();
+
+    // theme
+
+    let theme = match parsed.get("theme") {
+        Some(theme) => theme.as_str().map(String::from),
+        None => None,
+    };
+    let theme_str = theme.map(get_path);
 
     // get bars
 
@@ -109,37 +137,44 @@ pub fn parse_config(filename: &str) -> Config {
 
     // components
 
-    // theme
+    let component_option = parsed.get("component");
 
-    let theme = match parsed.get("theme") {
-        Some(theme) => theme.as_str().map(String::from),
-        None => None,
+    if let Some(Some(component_table)) = component_option.map(|d| d.as_table()) {
+        // get all component tables
+        let components: Vec<(&String, &Value)> = component_table
+            .iter()
+            .filter(|&(_k, v)| v.is_table())
+            .collect();
+        let component_configs: Vec<ComponentConfig> = components
+            .iter()
+            .map(|&(key, value)| {
+                let type_ = value.get("type");
+                if type_.is_none() {
+                    eprintln!("{}: component {} requires a type property", filename, key);
+                    exit(1i32);
+                }
+
+                ComponentConfig {
+                    name: key.to_string(),
+                    type_: String::from("poop"),
+                    properties: HashMap::new(),
+                }
+            })
+            .collect();
+
+        println!("{:#?}", component_configs);
     };
 
-    let theme_str = theme.clone().map(|x| {
-        // this can be improved
-        let config_dir = Path::new(filename).parent().unwrap();
-        let theme_path = Path::new(&x);
-        let theme_path = if theme_path.is_absolute() {
-            theme_path.to_path_buf()
-        } else {
-            config_dir.join(&theme_path)
-        }.canonicalize();
-        if theme_path.is_err() {
-            eprintln!("{}: {:?}", &x, theme_path.err().unwrap());
-            exit(2i32);
-        }
-        theme_path.unwrap().as_path().to_str().unwrap_or("").to_string()
-    });
 
     // root
 
     let config = Config {
         theme: theme_str,
         bars: bar_configs,
+        components: Vec::new(),
     };
 
-    println!("{:#?}", config);
+    // println!("{:#?}", config);
 
     config
 }
