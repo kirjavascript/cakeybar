19ab3dbe0a127cd17b9f8e476ff5de039fcc6b6b
kirjavascript <snkenjoi@gmail.com>
2018-06-04 22:11:14 +0100

    test

--- src/components/menu.rs
+++ src/components/menu.rs
@@ -6,18 +6,18 @@ use std::rc::Rc;
 
 use config::Property;
 
-pub struct Menu<'a, 'b> {
-    items: Vec<(&'a String, &'b String)>,
+pub struct Menu {
+    items: Vec<(String, String)>,
     is_open: bool,
 }
 
-impl<'a, 'b> Menu<'a, 'b> {
+impl Menu {
     fn toggle(&mut self) {
         self.is_open == !self.is_open;
     }
 }
 
-impl<'a, 'b> Component for Menu<'a, 'b> {
+impl Component for Menu {
     fn init(container: &gtk::Box, config: &ComponentConfig, _bar: &Bar) {
         let label = Label::new(None);
         Menu::init_widget(&label, &config);
@@ -29,35 +29,31 @@ impl<'a, 'b> Component for Menu<'a, 'b> {
         ebox.show_all();
 
         // get list of items
-        let default = (&"".to_string(), &"".to_string());
-        let items = config.get_vec_or("items", vec![]);
-        let items: Vec<(&String, &String)> = items
+        let mut items: Vec<(String, String)> = Vec::new();
+        config.get_vec_or("items", vec![])
             .iter()
-            .map(|x| {
+            .for_each(|x| {
                 if let Property::Array(val) = x {
                     let name_opt = val.get(0);
                     let exec_opt = val.get(1);
                     if let Some(Property::String(name)) = name_opt {
                         if let Some(Property::String(exec)) = exec_opt {
-                            return (name, exec);
+                            items.push((name.clone(), exec.clone()));
                         }
                     }
                 }
-                default
-            })
-            .filter(|x| x != &default)
-            .collect();
+            });
 
         let menu = Rc::new(RefCell::new(
-            Menu { items, is_open: false }
+            Menu { items: items, is_open: false }
         ));
 
         ebox.connect_button_press_event(move |_, _| {
             // let value = !*menu_open.borrow();
             // *menu_open.borrow_mut() = value;
             // println!("{:#?}", value);
-            // menu.borrow_mut().toggle();
-            // println!("{:#?}", menu.borrow().is_open);
+            menu.borrow_mut().toggle();
+            println!("{:#?}", menu.borrow().is_open);
             Inhibit(false)
         });
         // click event
