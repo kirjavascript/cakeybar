3fb9ef70162614f25e406823170db5fd5eaadf20
kirjavascript <snkenjoi@gmail.com>
2018-07-10 20:03:07 +0100

    finished window componant

--- src/wm/xcb/listen.rs
+++ src/wm/xcb/listen.rs
@@ -7,10 +7,6 @@ use std::sync::mpsc;
 use wm;
 use wm::events::{Event, EventValue};
 
-// TODO: non utf8 name, init, firefox tab change
-//
-// subscribe and unsubscrive from active windows
-
 pub fn listen(wm_util: &::wm::WMUtil) {
 
     let (tx, rx) = mpsc::channel();
@@ -36,7 +32,63 @@ pub fn listen(wm_util: &::wm::WMUtil) {
             let _wm_name = atoms.get(wm::atom::_NET_WM_NAME);
             let _utf8_string = atoms.get(wm::atom::UTF8_STRING);
 
-            // get current window (or 0 for now)
+            let mut current_window = xcb::NONE;
+
+            let mut get_title = |is_active: bool| {
+                let cookie = xcb::get_property(
+                    &conn,
+                    false,
+                    screen.root(),
+                    _active_window,
+                    xcb::ATOM_WINDOW,
+                    0,
+                    8,
+                );
+
+                match cookie.get_reply() {
+                    Ok(reply) => {
+                        let value: &[u32] = reply.value();
+                        let window = value[0];
+
+                        if is_active && current_window != window {
+                            // unsubscribe old window
+                            if current_window != xcb::NONE {
+                                xcb::change_window_attributes_checked(&conn, current_window, &[
+                                    (xcb::CW_EVENT_MASK, xcb::EVENT_MASK_NO_EVENT),
+                                ]);
+                            }
+                            // subscribe to new one
+                            if window != xcb::NONE {
+                                xcb::change_window_attributes_checked(&conn, window, &[
+                                    (xcb::CW_EVENT_MASK, xcb::EVENT_MASK_PROPERTY_CHANGE),
+                                ]);
+                            }
+                            current_window = window;
+                        }
+                        if window == xcb::NONE {
+                            tx.send(Ok(
+                                "".to_string()
+                            )).unwrap();
+                        } else {
+
+                            let title = wm::xcb::get_string(
+                                &conn,
+                                window,
+                                _utf8_string,
+                                _wm_name,
+                            );
+                            tx.send(Ok(
+                                title.trim().to_string()
+                            )).unwrap();
+                        }
+                    },
+                    Err(err) => {
+                        warn!("xcb cookie error {:?}", err);
+                    },
+                }
+            };
+
+            get_title(true);
 
             loop {
                 match conn.wait_for_event() {
@@ -49,51 +101,14 @@ pub fn listen(wm_util: &::wm::WMUtil) {
                                     xcb::cast_event(&event)
                                 };
                                 let event_atom = event.atom();
-                                let is_title = event_atom == _active_window
+                                let is_active = event_atom == _active_window;
+                                let is_title = is_active
                                     || event_atom == _current_desktop
                                     || event_atom == _visible_name
                                     || event_atom == _wm_name;
 
-
                                 if is_title {
-                                    let cookie = xcb::get_property(
-                                        &conn,
-                                        false,
-                                        screen.root(),
-                                        _active_window,
-                                        xcb::ATOM_WINDOW,
-                                        0,
-                                        8,
-                                    );
-
-                                    match cookie.get_reply() {
-                                        Ok(reply) => {
-                                            let value: &[u32] = reply.value();
-                                            let window = value[0];
-
-                                if event_atom == _active_window {
-                                    // unsubscribe and subscribe again
-                                }
-                                            if window == 0 {
-                                                tx.send(Ok(
-                                                    "".to_string()
-                                                )).unwrap();
-                                            } else {
-                                                let title = wm::xcb::get_string(
-                                                    &conn,
-                                                    window,
-                                                    _utf8_string,
-                                                    _wm_name,
-                                                );
-                                                tx.send(Ok(
-                                                    title.trim().to_string()
-                                                )).unwrap();
-                                            }
-                                        },
-                                        Err(err) => {
-                                            warn!("xcb cookie error {:?}", err);
-                                        },
-                                    }
+                                    get_title(is_active);
                                 }
                             },
                             _ => { },
@@ -105,7 +120,7 @@ pub fn listen(wm_util: &::wm::WMUtil) {
                     }
                 }
             }
-
+            warn!("this should never happen?");
         }
         else {
             tx.send(Err(format!("could not connect to X server"))).unwrap();
