daa8b2c8263e95e70552062e597def18c7e893ea
kirjavascript <snkenjoi@gmail.com>
2018-08-02 21:58:34 +0100

    token parser

--- misc/omnomnom/src/main.rs
+++ misc/omnomnom/src/main.rs
@@ -1,44 +1,37 @@
 #[macro_use]
 extern crate nom;
 
+use nom::types::CompleteStr as Input;
+
 #[derive(Debug)]
 enum Token {
     Text(String),
     Symbol(String),
 }
 
-named!(symbol<&str,Token>,
+
+named!(symbol<Input,Token>,
     map!(
-        delimited!(char!('{'), is_not!("{}"), char!('}')),
+        delimited!(char!('{'), ws!(is_not!("{}")), char!('}')),
         |s| Token::Symbol(s.to_string())
     )
 );
 
-named!(text<&str,Token>,
+named!(text<Input,Token>,
    map!(
        is_not!("{}"),
        |s| Token::Text(s.to_string())
    )
 );
 
-named!(get_token<&str,Token>,
-    alt!( symbol | text )
-);
-
-named!(get_tokens<&str,Vec<Token>,
-    many0!( get_token )
+named!(get_tokens<Input,Vec<Token>>,
+    many0!( alt!( symbol | text ) )
 );
 
 fn main() {
-    let mut input = " hello% {one} test {two} hello {one} poop  ";
-    let mut tokens: Vec<Token> = Vec::new();
-    loop {
-        if let Ok((rest, token)) = get_token(input) {
-            input = rest;
-            tokens.push(token);
-        } else {
-            break;
-        }
-    }
-    println!("{:#?}", tokens);
+    let input = Input("{one} hello% { one} test {two} hello {one} poop  ");
+    println!("{:#?}", get_tokens(input));
 }
+
+// tests
+// config.getFormat
