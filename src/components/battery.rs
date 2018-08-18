421299405e925becf3a3f238a155741d2f7a79f9
kirjavascript <snkenjoi@gmail.com>
2018-08-18 22:46:24 +0100

    testing on laptopw

--- src/components/battery.rs
+++ src/components/battery.rs
@@ -9,14 +9,10 @@ use systemstat::{System, Platform};
 
 pub struct Battery;
 
-fn get_data(device: &str, query: &str) -> Result<String, Error> {
-    read_file(&format!("/sys/class/power_supply/{}/{}", device, query))
-}
-
 impl Component for Battery {
     fn init(container: &gtk::Box, config: &ComponentConfig, bar: &Bar) {
 
-        // TODO: add duration, add symbols
+        // TODO: add duration
 
         let label = Label::new(None);
         Self::init_widget(&label, container, config, bar);
@@ -27,57 +23,44 @@ impl Component for Battery {
         let has_battery = get_data(&battery, "charge_full").is_ok();
 
         let symbols = SymbolFmt::new(config.get_str_or("format", "{percent}"));
-// {dplcsEnabled ? '✔' : '✗'}
-//
 
         if has_battery {
+        // if true {
             let tick = clone!(label move || {
-                let full = get_data(&battery, "full");
-                let now = get_data(&battery, "now");
-                let current = get_data(&battery, "current_now");
-                let data_all = full.map(|a| now.map(|b| current.map(|c| (a,b,c))));
-                if let Ok(a) = data_all {
+                if let Ok((full, now, current)) = get_charge(&battery) {
                     let plugged = get_data(&adapter, "online")
                         .unwrap_or("0".to_string()) == "1".to_string();
 
-                    println!("{:#?}", a);
-                    let q: () = a;
+                    // calculate pct
+                    let pct = now as f64 / full as f64 * 100.;
+                    let pct = pct.min(100.) as u8;
+                    // let is_full = pct >= 100;
 
+                    label.set_text(&symbols.format(|sym| match sym {
+                        "percent" => format!("{}%", pct),
+                        "plugged" => format!("{}", if plugged {'✔'} else {'✗'}),
+                        _ => sym.to_string(),
+                    }));
+
+                    // decide on class
+                    let class = match pct {
+                        p if p >= 99 => "full",
+                        p if p >= 65 => "high",
+                        p if p >= 30 => "medium",
+                        _ => "low",
+                    };
+
+                    // set classes
+                    if let Some(ctx) = label.get_style_context() {
+                        for class in ctx.list_classes().iter() {
+                            ctx.remove_class(&class);
+                        }
+                        if plugged {
+                            ctx.add_class("plugged");
+                        }
+                        ctx.add_class(class);
+                    }
                 }
-
-
-                // let v: Result<Vec<T>, Error>
-                    // let plugged = sys.on_ac_power().unwrap_or(true);
-                    // let capacity = life.remaining_capacity;
-                    // let time = life.remaining_time;
-                    // let is_full = capacity > 1.;
-                    // let pct = (capacity * 100.) as u8;
-
-                    // label.set_text(&symbols.format(|sym| match sym {
-                    //     "percent" => format!("{}%", pct),
-                    //     _ => sym.to_string(),
-                    // }));
-
-
-                    // // decide on class
-                    // let class = match pct {
-                    //     p if p >= 99 => "full",
-                    //     p if p >= 65 => "high",
-                    //     p if p >= 30 => "medium",
-                    //     _ => "low",
-                    // };
-
-                    // // set classes
-                    // if let Some(ctx) = label.get_style_context() {
-                    //     for class in ctx.list_classes().iter() {
-                    //         ctx.remove_class(&class);
-                    //     }
-                    //     if plugged {
-                    //         ctx.add_class("plugged");
-                    //     }
-                    //     ctx.add_class(class);
-                    // }
-                // }
                 gtk::Continue(true)
             });
 
@@ -88,46 +71,21 @@ impl Component for Battery {
             warn!("no battery detected");
         }
 
-        // let tick = clone!(label move || {
-        // let plugged = read_file(&get_path(adapter.clone(), "online"))
-        //     .unwrap_or("0".to_string()) == "1".to_string();
-
-
-        // // display percentage
-        // if let Ok(full) = charge_full {
-        //     let charge_now = read_file(&get_path(adapter.clone(), "charge_now"))
-        //         .unwrap_or("0".to_string()).parse::<u64>();
-        //     if let Ok(now) = charge_now {
-        //         // calculate pct
-        //         let pct = now as f64 / full as f64 * 100.;
-        //         let pct = pct.min(100.) as u8;
-        //         let is_full = pct >= 100;
-        //         let suffix = if is_full { "" } else { "%" };
-        //         label.set_text(&format!("{}{}", pct, suffix));
-
-        //         // decide on class
-        //         let class = match pct {
-        //             p if p >= 100 => "full",
-        //             p if p >= 65 => "high",
-        //             p if p >= 30 => "medium",
-        //             _ => "low",
-        //         };
-
-        //         // set classes
-        //         if let Some(ctx) = label.get_style_context() {
-        //             for class in ctx.list_classes().iter() {
-        //                 ctx.remove_class(&class);
-        //             }
-        //             if plugged {
-        //                 ctx.add_class("plugged");
-        //             }
-        //             ctx.add_class(class);
-        //         }
-        //     }
-        // }
-
-        // gtk::Continue(true)
-        // });
-
     }
+}
+
+fn get_data(device: &str, query: &str) -> Result<String, Error> {
+    read_file(&format!("/sys/class/power_supply/{}/{}", device, query))
+}
+
+fn get_charge(battery: &str) -> Result<(u64, u64, u64), Error> {
+    let full = get_data(battery, "full").map(|a| a.parse().unwrap_or(0));
+    let now = get_data(battery, "now").map(|a| a.parse().unwrap_or(0));
+    let current = get_data(battery, "current_now").map(|a| a.parse().unwrap_or(0));
+    // let full: Result<u64, ()> = Ok("6663000".to_string()).map(|a| a.parse().unwrap_or(0));
+    // let now: Result<u64, ()> = Ok("7368000".to_string()).map(|a| a.parse().unwrap_or(0));
+    // let current: Result<u64, ()> = Ok("1000".to_string()).map(|a| a.parse().unwrap_or(0));
+    full.and_then(|a| now.and_then(|b| current.and_then(|c| {
+        Ok((a, b, c))
+    })))
 }
