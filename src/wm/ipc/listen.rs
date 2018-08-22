6185f6d8127cc98d7bb3fa2f56a9d205079a2250
kirjavascript <snkenjoi@gmail.com>
2018-08-23 00:00:56 +0100

    askdjak,jshdkljah

--- src/wm/ipc/listen.rs
+++ src/wm/ipc/listen.rs
@@ -1,38 +1,46 @@
+use gtk;
 use wm::ipc;
 
 use std::thread;
 use std::os::unix::net::{UnixStream, UnixListener};
 use std::io::{Read, Write};
 use std::fs::remove_file;
-use std::sync::mpsc;
+use crossbeam_channel as channel;
 
 pub fn listen(wm_util: &::wm::WMUtil) {
     let socket_path = ipc::get_socket_path();
     // remove from last time
     remove_file(&socket_path).ok();
 
-    // thread::spawn(move || {
+    // start listening
+    let (s, r) = channel::unbounded();
+    thread::spawn(move || {
+        match UnixListener::bind(&socket_path) {
+            Ok(listener) => {
+                for connection in listener.incoming() {
+                    match connection {
+                        Ok(stream) => {
+                            thread::spawn(clone!(s || handle_stream(stream, s)));
+                        },
+                        Err(err) => {
+                            error!("IPC connection error: {}", err);
+                        }
+                    }
+                }
+            },
+            Err(err) => error!("Cannot start IPC"),
+        }
+    });
 
-    // });
-    // let (tx, rx) = mpsc::channel();
-    // match UnixListener::bind(&socket_path) {
-    //     Ok(listener) => {
-    //         for connection in listener.incoming() {
-    //             match connection {
-    //                 Ok(stream) => {
-    //                     thread::spawn(move || handle_stream(stream, tx));
-    //                 },
-    //                 Err(err) => {
-    //                     error!("IPC connection error: {}", err);
-    //                 }
-    //             }
-    //         }
-    //     },
-    //     Err(err) => error!("Cannot start IPC"),
-    // }
-    println!("{{:#?}}");
+    // receive events
+    gtk::timeout_add(10, move || {
+        if let Some(msg) = r.try_recv() {
+            println!("{:#?}", msg);
+        }
+        gtk::Continue(true)
+    });
 }
 
-fn handle_stream(stream: UnixStream, tx: mpsc::Sender<()>) {
+fn handle_stream(stream: UnixStream, s: channel::Sender<()>) {
 
 }
