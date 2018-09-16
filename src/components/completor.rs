f216a86554387b06d1f76bb0c574647733b71825
kirjavascript <snkenjoi@gmail.com>
2018-09-16 21:00:53 +0100

    some completor stuff

--- src/components/completor.rs
+++ src/components/completor.rs
@@ -33,8 +33,10 @@ impl Completor {
         window.set_type_hint(gdk::WindowTypeHint::Utility);
         window.set_skip_pager_hint(false);
         window.set_skip_taskbar_hint(false);
+        window.set_default_size(1, 1);
         window.set_decorated(false);
         window.set_title(&config.name);
+        window.stick();
         // super::init_widget(&wrapper, &config, bar, container);
 
         let entry = gtk::Entry::new();
@@ -44,7 +46,25 @@ impl Completor {
         window.add(&entry);
         bar.wm_util.add_window(&window);
         entry.grab_focus();
+        entry.connect_activate(move |e| {
+            println!("{:#?}", e.get_text());
+        });
 
+        entry.connect_key_press_event(move |e, t| {
+            println!("{:#?}", t);
+            gtk::Inhibit(false)
+        });
+
+        window.connect_focus_out_event(move |w, _| {
+            // println!("{:#?}", "out");
+            // w.grab_focus();
+            // w.show();
+            // entry.grab_focus();
+            w.destroy();
+            gtk::Inhibit(false)
+        });
+
+        // get window events
 
 
         // let d = gtk::Dialog::new();
