efa461fc012f7e00ea6ee3fe64668ca015f069be
kirjavascript <snkenjoi@gmail.com>
2018-09-23 15:50:52 +0100

    removed stripping

--- misc/build.sh
+++ misc/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cargo build --release
-strip target/release/cakeybar
+strip -s target/release/cakeybar
 du -h target/release/cakeybar
 help2man -N target/release/cakeybar > target/release/cakeybar.1
 # gzip target/release/cakeybar.1
